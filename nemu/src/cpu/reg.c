20d75cc60ad919a16edaf844c4ba2cebe082be81
tracer-ics2017 <tracer@njuics.org>
2018-08-30 15:23:51 +0800

    >  compile 121112 renming Linux renming-man 4.14.66-1-MANJARO #1 SMP PREEMPT Wed Aug 22 21:45:26 UTC 2018 x86_64 GNU/Linux  15:23:51 up  1:36,  1 user,  load average: 0.79, 0.99, 0.90 52e6263b7f145f03974f04035c68d5be68dcf55f

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -29,7 +29,9 @@ void reg_test() {
   assert(reg_b(R_CH) == ((sample[R_ECX] >> 8) & 0xff));
   assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
   assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
-  printf("EAX = %u  cpu.eax = %u", sample[0], cpu.eax);
+  char buffer [50];
+  sprintf(buffer,"EAX = %u  cpu.eax = %u", sample[0], cpu.eax);
+  printf("%s", buffer);
   assert(sample[R_EAX] == cpu.eax);
   assert(sample[R_ECX] == cpu.ecx);
   assert(sample[R_EDX] == cpu.edx);
