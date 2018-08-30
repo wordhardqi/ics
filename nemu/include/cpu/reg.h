d1bbd0fb31bea817bb0c2d06b90c1c4265a97e61
tracer-ics2017 <tracer@njuics.org>
2018-08-30 15:51:59 +0800

    >  compile 121112 renming Linux renming-man 4.14.66-1-MANJARO #1 SMP PREEMPT Wed Aug 22 21:45:26 UTC 2018 x86_64 GNU/Linux  15:51:59 up  2:05,  1 user,  load average: 1.27, 1.01, 0.92 a4dd68adc17f982b1cd883b61578b37f613d2d0b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,6 +15,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+  union{
   union {
     uint32_t _32;
     uint16_t _16;
@@ -26,8 +27,11 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
+  struct {
+  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  };
+  };
   vaddr_t eip;
 
 } CPU_state;
