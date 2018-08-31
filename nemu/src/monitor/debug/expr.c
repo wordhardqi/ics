6f3e3b8527ce11d4bbefa2f4bf08e3c328da069d
tracer-ics2017 <tracer@njuics.org>
2018-08-31 15:07:53 +0800

    >  compile 121112 renming Linux renming-man 4.14.66-1-MANJARO #1 SMP PREEMPT Wed Aug 22 21:45:26 UTC 2018 x86_64 GNU/Linux  15:07:53 up 17:57,  1 user,  load average: 0.81, 1.18, 1.22 518b978def1abd16a5c9b21cb24c60a1902ffa04

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ,TK_PLUS,TK_MINUS,TK_MULT,TK_DIV,TK_LP,TK_RP,TK_DIGITS
 
   /* TODO: Add more token types */
 
@@ -23,9 +23,23 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"==", TK_EQ} ,        // equal
+  {"\\+", TK_PLUS},         // plus
+  {"-",TK_MINUS},
+  {"\\*",TK_MULT},
+  {"/",TK_DIV},
+  {"\\(",TK_LP},
+  {"\\)",TK_RP},
+
+  {"[0-9]+",'d'},
 };
+int precedence(int token_type){
+  if(token_type == TK_PLUS || token_type ==TK_MINUS)
+  return 1;
+  if(token_type == TK_MULT || token_type == TK_DIV)
+  return 2;
+  return 0; 
+}
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
@@ -44,6 +58,7 @@ void init_regex() {
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+      
     }
   }
 }
@@ -53,6 +68,15 @@ typedef struct token {
   char str[32];
 } Token;
 
+int applyOp(int val1, int val2, Token* token){
+  switch (token->type){
+    case TK_PLUS: return val1 + val2;
+    case TK_MINUS: return val1 - val2;
+    case TK_MULT : return val1 * val2;
+    case TK_DIV : return val1 / val2;
+  }
+  // Log_write("error: unkonwn token %s", token->str);  
+}
 Token tokens[32];
 int nr_token;
 
@@ -73,6 +97,10 @@ static bool make_token(char *e) {
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
+        if(substr_len > 31){
+          return false;
+          // panic("Substr to long");
+        }
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
@@ -80,7 +108,23 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE: break;
+          case TK_EQ:
+          case TK_PLUS:
+          case TK_MINUS:
+          case TK_MULT:
+          case TK_DIV:
+          case TK_LP:
+          case TK_RP:
+              tokens[nr_token].type = rules[i].token_type;
+              strncpy(tokens[nr_token].str,substr_start,substr_len);
+              tokens[nr_token].str[substr_len]='\0';
+              ++nr_token;
+              break;
+          default:
+              break;
+
+
         }
 
         break;
@@ -103,7 +147,77 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  Token* opStack[32];
+  int valStack[32];
+  int opCur = 0;
+  int valCur = 0;
+  int i = 0;
+           int val;
 
-  return 0;
+  for(i=0; i<nr_token;++i){
+    Token* curToken = &tokens[i];
+    switch (curToken->type){
+      case TK_NOTYPE: break;
+      case TK_LP:
+          opStack[opCur]=curToken;
+          ++opCur;
+          break;
+      case TK_DIGITS:
+         sscanf(tokens[i].str,"%d", &val);
+         valStack[valCur] = val;
+         ++valCur;
+         break;
+      case TK_PLUS:
+      case TK_MINUS:
+      case TK_DIV:
+      case TK_MULT:
+          while(opCur !=0 
+            && precedence(opStack[opCur]->type)>= precedence(curToken->type) ){
+              int val2 = valStack[valCur];
+              --valCur;
+              int val1 = valStack[valCur];
+              --valCur;
+              Token* op = opStack[opCur];
+              --opCur;
+              valStack[valCur] = applyOp(val1,val2,op);
+              ++valCur;
+
+          }
+          opStack[opCur] = curToken;
+          ++opCur;
+          break;
+      case TK_RP:
+          while(opCur !=0 && opStack[opCur]->type != TK_LP){
+              int val2 = valStack[valCur];
+              --valCur;
+              int val1 = valStack[valCur];
+              --valCur;
+              Token* op = opStack[opCur];
+              --opCur;
+              valStack[valCur] = applyOp(val1,val2,op);
+              ++valCur;
+          }
+          --opCur;
+      default: break; 
+
+
+
+    }
+   
+
+
+  }
+   while(opCur !=0){
+       int val2 = valStack[valCur];
+              --valCur;
+              int val1 = valStack[valCur];
+              --valCur;
+              Token* op = opStack[opCur];
+              --opCur;
+              valStack[valCur] = applyOp(val1,val2,op);
+              ++valCur;
+    }
+    *success =  true;
+  return valStack[0];
+
 }
